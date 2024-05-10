145c333fdb7565db2b38c3f3b11baf80bec34f3c
James Kuang Zhongchuan <jameskuangzhongchuan@gmail.com>
2024-05-10 11:46:55 +0800

    implemented "cmds" command

--- main.c
+++ main.c
@@ -4,11 +4,26 @@
 */
 
 #include <stdio.h>
+#include <string.h>
 
-main() 
+int main() 
 {
-    char input;
+    char input[50];
+    int cmdsPage = 1;
 
     printf("Waiting for command... \nRun \"cmds\" for a list of commands.\n");
-    scanf("%c", &input);
+    scanf("%s", input);    
+
+    if (strcmp(input, "cmds") == 0) { // strcmp compares each character in the input string (an array), to those in the string (array) "char".
+        printf("=============================\n");
+        printf("LIST OF COMMANDS [Pg %d/1]\n", cmdsPage);
+        printf("=============================\n");
+        printf("./main - Initialises the main.c program.\n");
+        printf("cmds - Provides a list of commands.\n");
+        printf("list - Prints a list of students. Usage: list <page>\n");
+        printf("=============================\n");
+        printf("Run cmds <page> to view other pages.\n");
+        printf("=============================\n");
+    }
+    else printf("%s", input);
 }
