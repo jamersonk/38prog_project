a1c353d1e5cbaa43213122f8c0be92b20a40fdf9
James Kuang Zhongchuan <jameskuangzhongchuan@gmail.com>
2024-05-11 17:56:27 +0800

    implemented cmds & list command. modified welcome message. modified students array.

--- main.c
+++ main.c
@@ -10,21 +10,35 @@
 
 void main(void) 
 {
-    char *students[] = { "owien, hitler, owein" };
+    char *students[] = { "owien", "hitler", "owein", NULL };
+    int noStudents = 3; // figure out how to automatically increment later.
     char input[128] = "void";
 
+    printf("WELCOME TO THE STUDENT RECORD SYSTEM. v0.1");
+
     while (strcmp(input, "quit") != 0) {
-        printf("\nPlease enter a command. \"cmds\" for a list of commands. \"quit\" to exit the program.\n> ");
+        printf("\nPlease enter a command. Enter \"cmds\" for a list of commands; \"quit\" to exit.\n> ");
         scanf("%s", input);
 
-        if (strcmp(input, "quit") == 0)
+        if (strcmp(input, "quit") == 0 || strcmp(input, "close") == 0 || strcmp(input, "exit") == 0)
         {
             printf("Quitting!\n");
             return 0;
         }
         else if (strcmp(input, "cmds") == 0)
         {
-            printf("Placeholder\n");
+            printf("quit - exits the program. (sudos: close, exit)\n");
+            printf("cmds - lists all available commands.\n");
+            printf("list - lists all students in the record.\n");
+        }
+        else if (strcmp(input, "list") == 0)
+        {
+            int i = 0;
+            int count = 1;
+            while (i != noStudents) {
+                printf("%d %s \n", count, students[i]);
+                i += 1; count += 1;
+            }
         }
         else
         {
