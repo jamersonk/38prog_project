fe7a4ae0d9edfcb1582e9954035a54fce8f8125c
James Kuang Zhongchuan <jameskuangzhongchuan@gmail.com>
2024-05-15 18:46:41 +0800

    preliminary submission build

--- main.c
+++ main.c
@@ -20,38 +20,12 @@ struct Student
 
 int main() 
 {
-    // student array.
-    struct Student students[128] = { 
-        {.name = "Owein", .hasLastName = 1, .lastName = "BISMARK III", .id = 1, .gpa = 3.9},
-        {.name = "Bob", .hasLastName = 0, .id = 2, .gpa = 2.0},
-        {.name = "Owein", .hasLastName = 0, .id = 3, .gpa = 2.94},
-        NULL
-    };
-
-    int noStudents = 3; // figure out how to automatically increment later.
     char input[128] = "void";
 
     printf("WELCOME TO THE STUDENT RECORD SYSTEM v0.1");
 
     printf("\nPlease enter a command. Enter \"cmds\" for a list of commands; \"quit\" to exit.\n> ");
     scanf("%s", input);
-
-    if (strcmp(input, "quit") == 0 || strcmp(input, "close") == 0 || strcmp(input, "exit") == 0)
-    {
-        printf("Quitting!\n\n");
-         return 0;
-    }
-    else if (strcmp(input, "cmds") == 0)
-    {
-        printf("quit - exits the program. (sudos: close, exit)\n");
-        printf("cmds - lists all available commands.\n");
-        printf("list - lists all students in the record.\n");
-    }
-    else
-    {
-        printf("Invalid Command.\n");
-        return 1;
-    }   
 } 
 
 // functions will go here later
